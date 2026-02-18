1c236f6ca1b4f1e065b92031a3995fdf4546efd1
couper117 <ikennykelvin75@gmail.com>
2026-02-18 11:01:07 +0200

    5

--- admin/hash.php
+++ admin/hash.php
@@ -7,4 +7,4 @@ echo "Original Password: " . $password . "<br>";
 echo "MD5 Hashed Password: " . $hashedPassword;
 ?>
 the password is 12345678
-but it is not working i the login
+ the login is not working
